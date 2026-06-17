599098f3a1e368dabe25f8988a673f1fb8c76904
Arthur25880 <arthur.fariasmelo.funec@gmail.com>
2026-06-17 14:41:28 -0300

    Completar as alterações da dashboardadm

--- dashboardadm.php
+++ dashboardadm.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+$ocultarLogin = true;
+include 'index.html';
+?>
+
+<div class="acoes">
+    <a href="config.php">Configurações</a>
+    <a href="index.php" class="logout">Logout</a>
+</div>

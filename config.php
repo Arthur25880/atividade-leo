48ff0433629b17050c174d95c3b2809afd146abd
Arthur25880 <arthur.fariasmelo.funec@gmail.com>
2026-07-02 15:27:46 -0300

    oi

---
+++ config.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>config</title>
+    <style>
+        #quadrado{
+            background-color: blue;
+            width: 300px;
+            height: 300px;
+           align-content: center;
+        }
+        #escolher{
+            font-size: 250%;
+            color: greenyellow;
+        }
+    </style>
+</head>
+<body>
+    <center><div id="quadrado">
+        <a href="" id="escolher">CLIQUE</a>
+    </div></center>
+</body>
+</html>
+
+
+<?php
+
+
+
+?>
